210889b460ec2e27bf3b71e7ac5585ba775dfe3c
yuii <romain.styajkin@yandex.ru>
2026-09-07 12:30:39 +0300

    Добавлены docstrings

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,8 @@
+"""module implements text function"""
 def text(message: str) -> str:
+    """
+    Возвращает message
+
+    :param message: строка, которую нужно вывести в консоль
+    """
     return message
